c15b00adbe5ff3f80e648df8487339f707efb7b4
h4msterilla <h4msterilla@gmail.com>
2023-01-04 17:08:09 +0300

    add bd and set minimal config for webSecurity

---
+++ src/main/java/game/vt/silence/web/MainController.java
@@ -0,0 +1,14 @@
+package game.vt.silence.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String main(){
+        return "hello";
+    }
+
+}
