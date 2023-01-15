b9aac0a439a8254ec72dfcd65b32a23ad5b7b28e
h4msterilla <h4msterilla@gmail.com>
2023-01-15 07:48:02 +0300

    add JwtAuthFilter class

---
+++ src/main/java/game/vt/silence/security/jwt/JwtAuthFilter.java
@@ -0,0 +1,58 @@
+package game.vt.silence.security.jwt;
+
+import game.vt.silence.security.service.SecurityService;
+import game.vt.silence.security.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthFilter extends OncePerRequestFilter {
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
+    @Autowired
+    JwtUtil jwtUtil;
+
+    @Autowired
+    UserService userService;
+    @Autowired
+    SecurityService securityService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        /*if(request.getServletPath().equalsIgnoreCase("/logout")) {
+            jwtUtil.clearJwtHeader(response);
+            filterChain.doFilter(request, response);
+            return;
+        }*/
+
+        String jwt = parseJwt(request);
+
+        logger.info("get token: {}", jwt);
+
+        if (jwt != null && jwtUtil.validateJwtToken(jwt)) {
+            String username = jwtUtil.getUsernameFromJwtToken(jwt);
+            if (userService.findByUsername(username) != null)
+                securityService.nonPassAutoLogin(username, response);
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer "))
+            return headerAuth.substring(7, headerAuth.length());
+
+        return null;
+    }
+
+}
