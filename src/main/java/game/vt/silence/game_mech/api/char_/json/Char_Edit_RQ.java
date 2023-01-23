e8879f69bf7c8380b73efd984db29d1e4806544b
h4msterilla <h4msterilla@gmail.com>
2023-01-23 16:01:44 +0300

    add request class for /char/edit json

---
+++ src/main/java/game/vt/silence/game_mech/api/char_/json/Char_Edit_RQ.java
@@ -0,0 +1,10 @@
+package game.vt.silence.game_mech.api.char_.json;
+
+import lombok.Getter;
+
+@Getter
+public class Char_Edit_RQ {
+    String charname;
+    String skillname;
+    String value;
+}
