acfea1e6feaa2aa8598c65705b69ec723c21fc82
h4msterilla <h4msterilla@gmail.com>
2023-01-23 16:13:39 +0300

    add respond class for /char json

---
+++ src/main/java/game/vt/silence/game_mech/api/char_/json/Char_RS.java
@@ -0,0 +1,19 @@
+package game.vt.silence.game_mech.api.char_.json;
+
+import lombok.Getter;
+
+public class Char_RS {
+
+    Char_RS.Status status = new Char_RS.Status();
+
+    public Char_RS(String type, String message) {
+        status.type = type;
+        status.message = message;
+    }
+
+    @Getter
+    class Status {
+        String type;
+        String message;
+    }
+}
