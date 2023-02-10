72cd73846e8c91c6ca4243500d0c3180c41e2f35
h4msterilla <h4msterilla@gmail.com>
2023-02-10 11:53:22 +0300

    rename controller (1)

--- src/main/java/game/vt/silence/game_mech/api/char_/VTCharacterController.java
+++ src/main/java/game/vt/silence/game_mech/api/char_/VTCharacterController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class CharController {
+public class VTCharacterController {
 
     ObjectMapper jackson = new ObjectMapper();
     @Autowired
