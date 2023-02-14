5915638554562ea6b4425b343e8b6b561f8ff268
h4msterilla <h4msterilla@gmail.com>
2023-02-14 17:19:35 +0300

    refactoring. add upper service layer to avoid cycle dependency (1)

---
+++ src/main/java/game/vt/silence/beans/DefaultVTCharacterValuesLoader.java
@@ -0,0 +1,39 @@
+package game.vt.silence.beans;
+
+import game.vt.silence.game_mech.model.VTCharacterValue;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DefaultVTCharacterValuesLoader {
+    String valuesStr;
+
+    @SneakyThrows
+    public DefaultVTCharacterValuesLoader() {
+        File valuesFile = ResourceUtils.getFile("classpath:character-values");
+        InputStream in = new FileInputStream(valuesFile);
+        valuesStr = new String(in.readAllBytes());
+    }
+
+    public List<VTCharacterValue> getAsList() {
+        ArrayList<VTCharacterValue> vtCharacterValues = new ArrayList<>();
+
+        for (String line : valuesStr.split("\n")) {
+            if (line.startsWith("#")) continue;
+            String[] splitLine = line.split(",");
+            VTCharacterValue vtCharacterValue = new VTCharacterValue();
+            vtCharacterValue.setType(splitLine[0]);
+            vtCharacterValue.setName(splitLine[1]);
+            vtCharacterValue.setValue(0);
+            vtCharacterValues.add(vtCharacterValue);
+        }
+        return vtCharacterValues;
+    }
+}
