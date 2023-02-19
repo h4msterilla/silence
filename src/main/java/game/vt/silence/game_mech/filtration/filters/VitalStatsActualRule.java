29be70ba34f6e848d2739dd60f924a2607638da3
h4msterilla <h4msterilla@gmail.com>
2023-02-19 19:20:59 +0300

    add rule

---
+++ src/main/java/game/vt/silence/game_mech/filtration/filters/VitalStatsActualRule.java
@@ -0,0 +1,43 @@
+package game.vt.silence.game_mech.filtration.filters;
+
+import game.vt.silence.exceptions.VTCharacterValueBreakRuleException;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRule;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRulesChainState;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRulesChainStates;
+import game.vt.silence.game_mech.model.VTCharacterValue;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class VitalStatsActualRule implements VTCharacterValueRule {
+    @Override
+    public void doRule(Map<String, VTCharacterValue> vtValueMap, VTCharacterValue vtCharacterValue, int upDownArg, VTCharacterValueRulesChainState state) {
+
+        if(!(vtCharacterValue.getName().equals("vital_stats_health_actual") ||
+                vtCharacterValue.getName().equals("vital_stats_sanity_actual"))) return;
+
+        if(vtCharacterValue.getValue() + upDownArg < 0)
+            throw new VTCharacterValueBreakRuleException("this value can not be lower then 0");
+
+        if(vtCharacterValue.getName().equals("vital_stats_health_actual") &&
+                vtValueMap.get("vital_stats_health_max").getValue()
+                        + vtValueMap.get("stats_health_add").getValue()
+                        < vtCharacterValue.getValue() + upDownArg)
+            throw new VTCharacterValueBreakRuleException("actual health can not be higher then sum of max and add health");
+
+        if(vtCharacterValue.getName().equals("vital_stats_sanity_actual") &&
+                vtValueMap.get("vital_stats_sanity_max").getValue()
+                        + vtValueMap.get("stats_sanity_add").getValue()
+                        < vtCharacterValue.getValue() + upDownArg)
+            throw new VTCharacterValueBreakRuleException("actual health can not be higher then sum of max and add sanity");
+
+        vtCharacterValue.setValue(vtCharacterValue.getValue() + upDownArg);
+        state.setState(VTCharacterValueRulesChainStates.EDIT_UNLINKED);
+    }
+
+    @Override
+    public int getOrder() {
+        return 22;
+    }
+}
