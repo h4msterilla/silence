9a50fd7b51bf44c66a1069b219bd2da9443693a2
h4msterilla <h4msterilla@gmail.com>
2023-02-20 12:27:07 +0300

    add rule (4)

---
+++ src/main/java/game/vt/silence/game_mech/filtration/filters/ConnectionAndKnowledgeRule.java
@@ -0,0 +1,31 @@
+package game.vt.silence.game_mech.filtration.filters;
+
+import game.vt.silence.exceptions.VTCharacterValueBreakRuleException;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRule;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRulesChainState;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRulesChainStates;
+import game.vt.silence.game_mech.model.VTCharacterValue;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class ConnectionAndKnowledgeRule implements VTCharacterValueRule {
+    @Override
+    public void doRule(Map<String, VTCharacterValue> vtValueMap, VTCharacterValue vtCharacterValue, int upDownArg, VTCharacterValueRulesChainState state) {
+
+        if (!(vtCharacterValue.getType().equalsIgnoreCase("connections")
+                || vtCharacterValue.getType().equalsIgnoreCase("knowledge"))) return;
+
+        if (vtCharacterValue.getValue() + upDownArg < 0)
+            throw new VTCharacterValueBreakRuleException("this value can not be lower then 0");
+
+        vtCharacterValue.setValue(vtCharacterValue.getValue() + upDownArg);
+        state.setState(VTCharacterValueRulesChainStates.EDIT_UNLINKED);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
