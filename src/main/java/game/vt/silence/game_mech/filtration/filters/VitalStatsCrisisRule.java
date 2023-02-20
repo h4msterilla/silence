cd581c7aa1a89efee52b0c9921e39138422abac7
h4msterilla <h4msterilla@gmail.com>
2023-02-20 11:01:23 +0300

    add rule (1)

---
+++ src/main/java/game/vt/silence/game_mech/filtration/filters/VitalStatsCrisisRule.java
@@ -0,0 +1,31 @@
+package game.vt.silence.game_mech.filtration.filters;
+
+import game.vt.silence.exceptions.VTCharacterValueBreakRuleException;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRule;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRulesChainState;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRulesChainStates;
+import game.vt.silence.game_mech.model.VTCharacterValue;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class VitalStatsCrisisRule implements VTCharacterValueRule {
+    @Override
+    public void doRule(Map<String, VTCharacterValue> vtValueMap, VTCharacterValue vtCharacterValue, int upDownArg, VTCharacterValueRulesChainState state) {
+
+        if(!(vtCharacterValue.getName().equals("vital_stats_health_crisis") ||
+                vtCharacterValue.getName().equals("vital_stats_sanity_crisis"))) return;
+
+        if(vtCharacterValue.getValue() + upDownArg < 0)
+            throw new VTCharacterValueBreakRuleException("this value can not be lower then 0");
+
+        vtCharacterValue.setValue(vtCharacterValue.getValue() + upDownArg);
+        state.setState(VTCharacterValueRulesChainStates.EDIT_UNLINKED);
+    }
+
+    @Override
+    public int getOrder() {
+        return 23;
+    }
+}
