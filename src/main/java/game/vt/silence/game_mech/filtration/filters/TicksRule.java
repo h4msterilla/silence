d0a9ee4a88e4fc6de6f2beee2be19ca64838505e
h4msterilla <h4msterilla@gmail.com>
2023-02-20 12:22:21 +0300

    add rule (3)

---
+++ src/main/java/game/vt/silence/game_mech/filtration/filters/TicksRule.java
@@ -0,0 +1,30 @@
+package game.vt.silence.game_mech.filtration.filters;
+
+import game.vt.silence.exceptions.VTCharacterValueBreakRuleException;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRule;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRulesChainState;
+import game.vt.silence.game_mech.filtration.VTCharacterValueRulesChainStates;
+import game.vt.silence.game_mech.model.VTCharacterValue;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class TicksRule implements VTCharacterValueRule {
+    @Override
+    public void doRule(Map<String, VTCharacterValue> vtValueMap, VTCharacterValue vtCharacterValue, int upDownArg, VTCharacterValueRulesChainState state) {
+
+        if(!vtCharacterValue.getName().equalsIgnoreCase("value_ticks"))return;
+
+        if(vtCharacterValue.getValue() + upDownArg < 0)
+            throw new VTCharacterValueBreakRuleException("this value can not be lower then 0");
+
+        vtCharacterValue.setValue(vtCharacterValue.getValue() + upDownArg);
+        state.setState(VTCharacterValueRulesChainStates.EDIT_UNLINKED);
+    }
+
+    @Override
+    public int getOrder() {
+        return 4;
+    }
+}
