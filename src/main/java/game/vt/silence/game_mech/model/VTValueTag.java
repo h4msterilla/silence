44032d3a9718f9624dafe4bb99cd9e20221e66c1
h4msterilla <h4msterilla@gmail.com>
2023-03-26 21:09:19 +0300

    add tags to VTValue

---
+++ src/main/java/game/vt/silence/game_mech/model/VTValueTag.java
@@ -0,0 +1,18 @@
+package game.vt.silence.game_mech.model;
+
+public enum VTValueTag {
+    EXPERIENCE,
+    VITAL_STATE,
+    HEALTH,
+    SANITY,
+    BLOCK,
+    SKILL,
+    SPECIAL_SKILL,
+    ENDURANCE,
+    SWIFTNESS,
+    MIND,
+    EGO,
+    CONNECTIONS,
+    KNOWLEDGE,
+    CONDITIONS_AND_PROCESSES
+}
