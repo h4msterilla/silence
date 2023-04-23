e47580f9bf221b609417580408c0a353f8a9eb8d
h4msterilla <h4msterilla@gmail.com>
2023-04-23 03:11:48 +0300

    remove old comments

--- src/main/java/game/vt/silence/vaadin/LogRegMenu.java
+++ src/main/java/game/vt/silence/vaadin/LogRegMenu.java
@@ -1,14 +1,11 @@
 package game.vt.silence.vaadin;
 
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.Lumo;
 
 @Route("")
 public class LogRegMenu extends VerticalLayout {
@@ -20,20 +17,10 @@ public class LogRegMenu extends VerticalLayout {
     private Tabs tabs = new Tabs(loginTab, regTab);
 
     boolean darkTheme = true;
-    private Button themeButton = /*new Button("", e -> {
-        darkTheme = !darkTheme;
-        if (darkTheme)
-            getThemeList().set(Lumo.DARK, true);
-        else
-            getThemeList().set(Lumo.DARK, false);
-    });*/ new ThemeButton(/*this*/);
+    private Button themeButton = new ThemeButton();
 
     public LogRegMenu() {
-
-        //themeButton.setIcon(new Icon(VaadinIcon.ADJUST));
-
         setSizeFull();
-        //getThemeList().set(Lumo.DARK, true);
 
         HorizontalLayout horizontalLayout = new HorizontalLayout(themeButton);
         add(horizontalLayout);
